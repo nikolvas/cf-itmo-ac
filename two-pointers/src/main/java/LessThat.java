6a7035a7129d0fbd464869f08d8cc325ac19d2db
nikolvas <vasiliy.nikolenko@gmail.com>
2022-01-29 23:46:17 +0300

    Merge, lessThan

---
+++ two-pointers/src/main/java/LessThat.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+
+public class LessThat implements Runnable {
+    public static void main(String[] args) throws IOException {
+        new Thread(null, new LessThat(), "Main", 1 << 26).start();
+    }
+
+    @Override
+    public void run() {
+        FastScanner scanner = new FastScanner();
+        int n = 0, m = 0;
+        try {
+            n = scanner.nextInt();
+            m = scanner.nextInt();
+            int[] ns = new int[n];
+            for (int i = 0; i < n; i++) {
+                ns[i] = scanner.nextInt();
+            }
+            int[] ms = new int[m];
+            for (int i = 0; i < m; i++) {
+                ms[i] = scanner.nextInt();
+            }
+            System.out.println(Arrays.stream(lessThat(ns, ms)).mapToObj(Integer::toString).collect(Collectors.joining(" ")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int[] lessThat(int[]n, int[]m){
+        int nL = n.length;
+        int mL = m.length;
+        int[] res = new int[mL];
+        int i = 0;
+        for (int k = 0; k < mL; k++) {
+            while(i < nL && n[i] < m[k]){
+                i++;
+            }
+            res[k] = i;
+        }
+        return res;
+    }
+
+
+    public static class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        public FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
+}
